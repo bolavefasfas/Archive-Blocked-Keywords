ce57f7651ce11896fb9bf5f1ace7ba61e4a445f0
bolavefasfas <82230241+bolavefasfas@users.noreply.github.com>
2023-03-30 21:37:03 +0530

    Update blocklist.js

--- blocklist.js
+++ blocklist.js
@@ -1,1 +1,1 @@
-var blocked_keywords = ["porn", "fuck", "fucking", "fucked", "xxx", "brazzers", "pornhub", "sucking", "dick", "pussy", "licking"]
+var blocked_keywords = ["porn", "fuck", "fucking", "fucked", "xxx", "brazzers", "pornhub", "sucking", "dick", "pussy", "licking", "gay", "lesbain", "nude", "shemale", "blowjob", "handjob"]
